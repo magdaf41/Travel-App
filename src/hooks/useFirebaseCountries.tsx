8ce402d70823633ad5a3d5eafba365a201cacd2d
Magdalena Falkowska <magda.f41@gmail.com>
2025-02-27 14:45:20 +0100

    fix: improvements

--- src/hooks/useFirebaseCountries.tsx
+++ src/hooks/useFirebaseCountries.tsx
@@ -1,23 +1,17 @@
 import { useGetFirebaseApiQuery } from '@/api/firebaseApi.api'
-import ErrorMessage from '@/components/errors/CountriesDataErrorMessage'
 import LoadingIndicator from '@/components/loader/LoadingIndicator'
-import { CountriesDataTypes } from '@/types/CountriesData'
-import { ReactNode } from 'react'
+import ErrorMessage from '@/components/errors/CountriesDataErrorMessage'
 
-interface FirebaseCountriesResponse {
-	content: ReactNode | null
-	data: CountriesDataTypes[]
-}
-
-export const useFirebaseCountries = (): FirebaseCountriesResponse => {
+export const useFirebaseCountries = () => {
 	const { data, isLoading, error } = useGetFirebaseApiQuery()
-	let content: ReactNode | null = null
 
 	if (isLoading) {
-		content = <LoadingIndicator />
-	} else if (error) {
-		content = <ErrorMessage error={error} />
+		return { content: <LoadingIndicator />, data: [] }
 	}
 
-	return { content, data: data ?? [] } 
+	if (error) {
+		return { content: <ErrorMessage error={error} />, data: [] }
+	}
+
+	return { content: null, data: data ?? [] }
 }
